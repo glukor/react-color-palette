4a42e8cc5df7517fb3d9fb6db14d53ab18e0d974
Clément Franch <cfranch@clever-age.com>
2020-11-14 20:35:09 +0100

    Add NewPaletteForm and Link

---
+++ src/NewPaletteForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewPaletteform extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Palette form yeah</h1>
+            </div>
+        )
+    }
+}
+
+export default NewPaletteform;
